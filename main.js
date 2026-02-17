4e15bb3d6422730c68f4e501cef3c0aae7434008
johnnguyen <johnnguyen@uiowa.edu>
2026-02-17 15:48:31 -0600

    first push

---
+++ main.js
@@ -0,0 +1,42 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+//the unit values
+class Corps {
+    constructor(x,y, color){
+        this.x = x;
+        this.y = y;
+        this.size = 40;
+        this.color = color;
+        this.speed = 1;
+    }
+    // unit's movement to target
+    update(target) {
+        if (this.x < target.x) this.x += this.speed;
+        if (this.x > target.x) this.x -= this.speed;
+    }
+    // makes this unit square
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
+//unit's starting location and color
+const red = new Corps(100, 200, "red");
+const blue = new Corps(600, 400, "blue");
+
+function gameLoop() {
+    ctx.clearRect (0,0, canvas.width, canvas.height);
+
+    // movement
+    red.update(blue);
+    blue.update(red);
+
+    // puts them into their position
+    red.draw();
+    blue.draw();
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
